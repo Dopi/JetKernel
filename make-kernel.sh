773366770d5a70bb1a264ee8bac812f224798e56
Dopi <dopi711@googlemail.com>
2010-06-08 09:34:07 +0200

    Added call to initrd script in make-kernel.sh. FIXME: May not yet work for everyone as this depends on additional android source from the M900 package

--- make-kernel.sh
+++ make-kernel.sh
@@ -1,3 +1,5 @@
 #/bin/bash
-CCOMPILER=../../../android/prebuilt/linux-x86/toolchain/arm-eabi-4.3.1/bin/arm-eabi-
+CCOMPILER=../android/prebuilt/linux-x86/toolchain/arm-eabi-4.3.1/bin/arm-eabi-
+ANDROIDSRC=../SPH_M900_MR2_Kernel/src/open_src
+scripts/gen_initramfs_list.sh -u 0 -g 0 $ANDROIDSRC/out/target/product/SPH-M900/root/ > .initram_list
 make CROSS_COMPILE=$CCOMPILER
