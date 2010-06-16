211b134ab642aecf9ab31e0d09cdf78a61d015ec
Dopi <dopi711@googlemail.com>
2010-06-16 22:08:01 +0200

    small changes in build environment

---
+++ build.sh
@@ -0,0 +1,5 @@
+#/bin/bash
+ANDROIDSRC=../JetAndroid
+CCOMPILER=$ANDROIDSRC/prebuilt/linux-x86/toolchain/arm-eabi-4.3.1/bin/arm-eabi-
+scripts/gen_initramfs_list.sh -u 0 -g 0 $ANDROIDSRC/out/target/product/GT-I5700/root/ > .initram_list
+make CROSS_COMPILE=$CCOMPILER
