acb5d0e0be361443b266d9cd24591d19387d960c
Dopi <dopi711@googlemail.com>
2010-06-16 22:06:42 +0200

    small changes to build environment

--- make-kernel.sh
+++
@@ -1,5 +0,0 @@
-#/bin/bash
-CCOMPILER=../android/prebuilt/linux-x86/toolchain/arm-eabi-4.3.1/bin/arm-eabi-
-ANDROIDSRC=../JetBuild/open_src
-scripts/gen_initramfs_list.sh -u 0 -g 0 $ANDROIDSRC/out/target/product/SPH-M900/root/ > .initram_list
-make CROSS_COMPILE=$CCOMPILER
