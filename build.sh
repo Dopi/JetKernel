e793876884ad8f84067dd954ad74ed751395d2ad
Dopi <dopi711@googlemail.com>
2010-06-24 17:24:40 +0200

    removed initrd support from kernel config.

--- build.sh
+++ build.sh
@@ -2,5 +2,6 @@
 ANDROIDSRC=../android
 CCOMPILER=$ANDROIDSRC/prebuilt/linux-x86/toolchain/arm-eabi-4.3.1/bin/arm-eabi-
 #scripts/gen_initramfs_list.sh -u 0 -g 0 $ANDROIDSRC/out/target/product/GT-I5700/root/ > .initram_list
-scripts/gen_initramfs_list.sh -u 0 -g 0 $ANDROIDSRC/out/target/product/GT-S8000/root/ > .initram_list
+#scripts/gen_initramfs_list.sh -u 0 -g 0 $ANDROIDSRC/out/target/product/GT-S8000/root/ > .initram_list
+touch .initram_list
 make CROSS_COMPILE=$CCOMPILER
