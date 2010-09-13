68076f8deb494900132827d4b103de10adb407d8
Dopi <dopi711@googlemail.com>
2010-09-13 08:22:55 +0200

    added i2c4 decription. Not active yet as PHONE_RST_N GPIO needs to be moved

--- arch/arm/mach-s3c6410/mach-jet.c
+++ arch/arm/mach-s3c6410/mach-jet.c
@@ -160,7 +160,22 @@ static struct platform_device s3c_device_i2c3 = {
 	.dev.platform_data	= &i2c3_platdata,
 };
 
-// 4 USBSW_SCL_3.0V
+#if 0
+static struct i2c_gpio_platform_data i2c4_platdata = {
+	.sda_pin		= GPIO_USBSW_SDA_3V0,
+	.scl_pin		= GPIO_USBSW_SCL_3V0,
+	.udelay			= 2,	/* 250kHz */ 	//3,	/* 166KHz */		
+	.sda_is_open_drain	= 0,
+	.scl_is_open_drain	= 0,
+	.scl_is_output_only	= 1,
+};
+
+static struct platform_device s3c_device_i2c4 = {
+	.name				= "i2c-gpio",
+	.id					= 4,
+	.dev.platform_data	= &i2c4_platdata,
+};
+#endif
 
 static struct i2c_gpio_platform_data i2c5_platdata = {
 	.sda_pin		= GPIO_AP_SDA_1V8,
